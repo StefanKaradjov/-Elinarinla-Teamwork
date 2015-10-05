a8f8cd71c7b14b168b26a58439ecf4e3a1bba621
SashoStefanov <al.stefanov@yahoo.com>
2015-10-05 18:12:17 +0300

    You Lost Title

---
+++ YouLost/YouLose.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class YouLose
+    {
+        static void Main()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            string youLose = new string(' ', Console.WindowWidth / 2 - 59 / 2) + "*   *" + "  " + " ** " + "  " + "*   *" + "   " + "*   " + "  " + " ** " + "  " + new string('*', 4) + "  " + new string('*', 4) + "\n" +
+                          new string(' ', Console.WindowWidth / 2 - 59 / 2) + " * * " + "  " + "*  *" + "  " + "*   *" + "   " + "*   " + "  " + "*  *" + "  " + "*   " + "  " + "*   " + "\n" +
+                          new string(' ', Console.WindowWidth / 2 - 59 / 2) + "  *  " + "  " + "*  *" + "  " + "*   *" + "   " + "*   " + "  " + "*  *" + "  " + new string('*', 4) + "  " + new string('*', 4) + "\n" +
+                          new string(' ', Console.WindowWidth / 2 - 59 / 2) + "  *  " + "  " + "*  *" + "  " + "*   *" + "   " + "*   " + "  " + "*  *" + "  " + "   *" + "  " + "*   " + "\n" +
+                          new string(' ', Console.WindowWidth / 2 - 59 / 2) + "  *  " + "  " + " ** " + "  " + " ***  " + "  " + new string('*', 4) + "  " + " ** " + "  " + new string('*', 4) + "  " + new string('*', 4) + "\n";
+
+
+
+            Console.WriteLine(youLose);
+        }
+    }
