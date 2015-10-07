26bdc0b6106e4f54f197277f75b1614eb72f7aa5
SashoStefanov <al.stefanov@yahoo.com>
2015-10-07 20:18:37 +0300

    Probe

---
+++ HangmanGameCode1/Hangman1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class Hangman1
+    {
+
+public void Play()
+{
+    string guessed_FoundLetters = new List<string>();
+ 
+    for (int i = 0; i < PickedWord.WordLength; i++)
+    // Add underscore to the guessed and found string collection
+    {
+        guessed_FoundLetters.Add(" _ ");
+    }
+ 
+    for (int i = 0; i < PickedWord.WordLength; i++)
+    {
+        string letter = PickedWord.Content.Substring(i, 1);
+        if (GuessedLetters.Count > 0)
+        {
+            foreach (string guessedLetter in this.GuessedLetters)
+            {
+                if (letter.Equals(guessedLetter.Trim().ToUpper()))
+                // If the guessed letter is found from the picked
+                // word then replace underscore with the letter
+                {
+                    guessed_FoundLetters.RemoveAt(i);
+                    guessed_FoundLetters.Insert(i, " " + letter + " ");
+                }
+            }
+        }
+    }
+    drawHangMan();
+    Console.WriteLine(buildString(guessed_FoundLetters, false));
+    Console.WriteLine();
+}
+        }
+
